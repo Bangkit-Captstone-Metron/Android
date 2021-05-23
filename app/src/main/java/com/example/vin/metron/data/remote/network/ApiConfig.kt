ba356368cdd071889377293c3fd24db1cffeca64
unknown <Sthajeb1@gmail.com>
2021-05-24 05:17:55 +0700

    feat: set up retrofit + repository pattern

---
+++ app/src/main/java/com/example/vin/metron/data/remote/network/ApiConfig.kt
@@ -0,0 +1,27 @@
+package com.example.vin.metron.data.remote.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ApiConfig {
+    private fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+
+    fun provideApiService(): ApiService {
+        val retrofit = Retrofit.Builder()
+            //Todo: adjust the base URL with Cloud REST API.
+            .baseUrl("")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(provideOkHttpClient())
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+}
